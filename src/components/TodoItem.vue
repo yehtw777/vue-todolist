64edfb0e002dd4b98adecf7d1f99527776c1c242
BR <br@BRdeMacBook-Pro.local>
2021-01-12 22:38:37 +0800

    completed Todoitem

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,13 +1,23 @@
 <script>
+// import { component } from "vue/types/umd";
+// generate automately, that cause 98% after emitting CopyPlugin
 export default {
   name: "todoitem",
+  props: ["content", "index"],
+  methods: {
+    handleDelete() {
+      console.log("handleDelete");
+      this.$emit("delete", this.index);
+      // send index value to parent component
+    },
+  },
 };
 </script>
 
 <template>
   <div>
-    <h2>TodoItem is me!!!!!</h2>
-    <li></li>
+    <!-- <h2>TodoItem is me!!!!!</h2> -->
+    <li @click="handleDelete">{{ content }}</li>
   </div>
 </template>
 
